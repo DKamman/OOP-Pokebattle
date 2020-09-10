3b23e1620fd2cfdf243aa30b156e69c45e1af78b
DKamman <42833740+DKamman@users.noreply.github.com>
2020-09-10 13:59:54 +0200

    Quick update

--- index.php
+++ index.php
@@ -2,14 +2,8 @@
 
 require 'init.php';
 
-$pikachu = new Pikachu('token');
+$pikachu = new Pikachu('Makinto');
 
-echo $pikachu->getName();
-echo $pikachu->getEnergyType();
-echo $pikachu->getHitpoints();
-
-$charmeleon = new Charmeleon('Token');
-
-echo $charmeleon->getName();
-echo $charmeleon->getEnergyType();
-echo $charmeleon->getHitpoints();
+echo $pikachu->getName() . " ";
+echo $pikachu->getEnergyType() . " "; 
+echo $pikachu->getHitpoints() . " ";
