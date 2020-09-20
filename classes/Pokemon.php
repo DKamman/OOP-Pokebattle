83659e00380ba70530601a87f2a1cc6c162ae639
DKamman <42833740+DKamman@users.noreply.github.com>
2020-09-21 01:37:20 +0200

    Updated attackPokemon function

--- classes/Pokemon.php
+++ classes/Pokemon.php
@@ -31,31 +31,24 @@ class Pokemon {
         }
     }
 
-    
-    // $headbuttAttack = $attacks["HEADBUTT"]; 
-    // $attacks = array("HEADBUTT" => "Head Butt", 50);
-    // Associative array to use for the attackPokemon function. To be able to specify by name what attack to use.
-
     public function attackPokemon($attack, $target){
         echo "<br><hr>Attacking<br>Pokémon: " . $this->name . " " . $this->pokemonType . " " . $this->energyType . ", Attack damage: " . $attack->damage . "<br><br>";
         echo "Defending<br>Pokémon: " . $target->name . " " . $target->pokemonType . " " . $target->energyType . ", Health: " . $target->health . ", Resistance: " . $target->resistance->energyType . ", Weakness: " . $target->weakness->energyType . "<br><br>";
 
         // // Weakness calculation
-        // if energytype of attacker == energytype of the target's weakness, attacker damage * weakness multiplier
         if ($this->energyType == $target->weakness->energyType) {
             $actualDamage = $attack->damage * $target->weakness->multiplier;
             echo "The attack did " . $actualDamage . " damage to " . $target->name . ".<br>";
             $actualHealth = $target->health - $actualDamage;
-            echo "Actual health: " . $actualHealth . "<hr><br><br>";
+            echo $target->name . "'s health: " . $actualHealth . "<hr><br><br>";
         }
 
         // // Resistance calculation
-        // if energytype of attacker == energytype of the target's resistance, attacker damage minus resistance value
         if ($this->energyType == $target->resistance->energyType) {
             $actualDamage = $attack->damage - $target->resistance->value;
             echo "The attack did " . $actualDamage . " damage to " . $target->name . ".<br>";
             $actualHealth = $target->health - $actualDamage;
-            echo "Actual health: " . $actualHealth . "<hr><br><br>";
+            echo $target->name . "'s health: " . $actualHealth . "<hr><br><br>";
         } 
     }
 
